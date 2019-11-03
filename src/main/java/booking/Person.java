142d2eb53b31185b5ea8e2c54a874bf513f3cdc1
Javid <javid.mammadli99@gmail.com>
2019-11-03 13:23:05 +0400

    - booking package

---
+++ src/main/java/booking/Person.java
@@ -0,0 +1,11 @@
+package booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("adad");
+        List<Integer> l=new ArrayList<>();
+    }
+}
