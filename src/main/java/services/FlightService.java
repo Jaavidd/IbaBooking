275d43fda71a0982d7f37106f4fd223c5cfb8c90
Aleksey Hajialiyev <54638038+Yeskela7@users.noreply.github.com>
2019-11-13 14:06:35 +0400

    Update FlightService.java

--- src/main/java/services/FlightService.java
+++ src/main/java/services/FlightService.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 public class FlightService {
 
 
-    private Dao<Flight> flightDao = new DataFlight();
+    private Dao<Flight> flightDao = new StorageFlights();
 
     public FlightService() throws IOException {
     }
